1cefea46b40fdbc7d0b20b9b6ac7ff83ac878c86
luccessn <gulualuka0@gmail.com>
2025-02-23 06:27:52 -0800

    eslintcommit

--- src/Components/AboutUs/AboutUs.jsx
+++ src/Components/AboutUs/AboutUs.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-no-target-blank */
+/* eslint-disable jsx-a11y/iframe-has-title */
 import React from "react";
 import createGlobe from "cobe";
 import { useEffect, useRef } from "react";
